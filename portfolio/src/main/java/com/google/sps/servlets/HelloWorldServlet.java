ce241dd5971074c5440bd104145b89f8e5833c3d
Diana Espindola <dianae2194@gmail.com>
2021-03-17 01:51:29 +0000

    Week 2 Project 1.

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -11,8 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloWorldServlet extends HttpServlet {
 
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  public void doGet(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h1>Dif</h1>");
   }
 }
